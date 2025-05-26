13362dba9f84863fcb4862eee8e914b19f03a871
0529Koki <kouki_23s1102728@nnn.ed.jp>
2025-05-26 23:40:36 +0900

    新しいhtml,css追加

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,9 @@
+app_name = 'todo'
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create_task, name='create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'todo/index.html')
+def create_task(request):
+    # POSTで来たタスクの処理を書くところ
+    pass
